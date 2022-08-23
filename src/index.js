9ea921ceecb83cf1a106d7fa196587ff5506fb7c
Shehan Gunasekara <87413135+it21011870@users.noreply.github.com>
2022-08-23 15:38:40 +0530

    a

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
     <h1>Hello world</h1>
-    <p>This is paragraphaaaaaaaaaaaaaaaaaaaaaa</p>
+    <p>This is paragraph</p>
   </div>,
   document.getElementById("root")
 );
